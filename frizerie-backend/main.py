03e8c2a887ab0d763f8587a22aae242533ac0c36
mrarmas02 <alexandruarmas02@gmail.com>
2025-05-28 03:15:13 +0300

    Fix: Use config=Config(environ=os.environ) for SlowAPI Limiter

--- frizerie-backend/main.py
+++ frizerie-backend/main.py
@@ -141,7 +141,7 @@ try:
             }
         )
 
-    limiter = Limiter(key_func=get_remote_address, default_limits=["5/second"])
+    limiter = Limiter(key_func=get_remote_address, default_limits=["5/second"], config=Config(environ=os.environ))
 
     app.state.limiter = limiter
     app.add_middleware(SlowAPIMiddleware)
